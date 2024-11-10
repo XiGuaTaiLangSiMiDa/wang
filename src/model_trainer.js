c9dfe29dd221665d2811bc00275ea0736d293c69
XiGuaTaiLangSiMiDa <xiguatailangsimida@gmail.com>
2024-11-10 17:30:22 +0800

    x

--- src/model_trainer.js
+++ src/model_trainer.js
@@ -46,6 +46,16 @@ class ModelTrainer {
         return model;
     }
 
+    // 标准化数据
+    normalizeData(data) {
+        const mean = data.mean(0);
+        const std = data.std(0);
+        // 防止除以0，添加一个小的epsilon值
+        const epsilon = tf.scalar(1e-7);
+        const normalizedData = data.sub(mean).div(std.add(epsilon));
+        return { normalizedData, mean, std };
+    }
+
     // 准备训练数据
     prepareData(features, labels) {
         // 保存特征名称
@@ -59,14 +69,13 @@ class ModelTrainer {
         const ys = tf.tensor2d(labels, [labels.length, 1]);
         
         // 标准化特征
-        const { dataMean, dataStd } = tf.moments(xs, 0);
-        const normalizedXs = xs.sub(dataMean).div(dataStd.add(tf.scalar(1e-6)));
+        const { normalizedData, mean, std } = this.normalizeData(xs);
         
         return {
-            xs: normalizedXs,
+            xs: normalizedData,
             ys,
-            dataMean,
-            dataStd
+            dataMean: mean,
+            dataStd: std
         };
     }
 
@@ -99,7 +108,7 @@ class ModelTrainer {
 
         // 计算特征重要性
         console.log('计算特征重要性...');
-        this.calculateFeatureImportance(xs, ys);
+        await this.calculateFeatureImportance(xs, ys);
 
         return {
             model: this.model,
@@ -110,9 +119,10 @@ class ModelTrainer {
     }
 
     // 计算特征重要性
-    calculateFeatureImportance(xs, ys) {
+    async calculateFeatureImportance(xs, ys) {
         const baselinePred = this.model.predict(xs);
-        const baselineLoss = tf.losses.meanSquaredError(ys, baselinePred);
+        const baselineLoss = tf.metrics.binaryCrossentropy(ys, baselinePred);
+        const baselineLossValue = await baselineLoss.data();
         
         this.featureImportance = [];
         
@@ -126,23 +136,35 @@ class ModelTrainer {
             
             // 计算新的预测和损失
             const newPred = this.model.predict(shuffledXs);
-            const newLoss = tf.losses.meanSquaredError(ys, newPred);
+            const newLoss = tf.metrics.binaryCrossentropy(ys, newPred);
+            const newLossValue = await newLoss.data();
             
             // 特征重要性 = 打乱后损失 - 基准损失
-            const importance = newLoss.sub(baselineLoss).dataSync()[0];
+            const importance = newLossValue[0] - baselineLossValue[0];
             
             this.featureImportance.push({
                 feature: this.featureNames[i],
                 importance: importance
             });
+
+            // 清理内存
+            shuffledXs.dispose();
+            col.dispose();
+            shuffledCol.dispose();
+            newPred.dispose();
+            newLoss.dispose();
         }
+        
+        // 清理内存
+        baselinePred.dispose();
+        baselineLoss.dispose();
         
         // 按重要性排序
         this.featureImportance.sort((a, b) => b.importance - a.importance);
     }
 
     // 预测单个样本
-    predict(features) {
+    async predict(features) {
         if (!this.model) {
             throw new Error('Model not trained yet');
         }
@@ -151,7 +173,13 @@ class ModelTrainer {
         const xs = tf.tensor2d(featureArray);
         
         const prediction = this.model.predict(xs);
-        return prediction.dataSync()[0];
+        const probability = await prediction.data();
+        
+        // 清理内存
+        xs.dispose();
+        prediction.dispose();
+        
+        return probability[0];
     }
 
     // 保存模型
@@ -166,6 +194,13 @@ class ModelTrainer {
     async loadModel(path) {
         this.model = await tf.loadLayersModel(`file://${path}`);
         return this.model;
+    }
+
+    // 清理资源
+    dispose() {
+        if (this.model) {
+            this.model.dispose();
+        }
     }
 }
 
